f0a557769c8a9dbb14941256d89cbba5b895f496
Rohini1509 <64795200+Rohini1509@users.noreply.github.com>
2021-03-16 20:10:25 +0530

    Add files via upload

---
+++ divermovement.js
@@ -0,0 +1,37 @@
+function diver_moves()
+{
+
+    if(keyDown(LEFT_ARROW))
+    {
+      john.changeAnimation("swimmingleft",swimmingleft)
+      netsprite.changeImage('netleft',netimage)
+      john.x = john.x - 8
+      netsprite.x = john.x - 200;
+      netsprite.y = john.y+50
+      
+    }
+    
+    if(keyDown(UP_ARROW))
+    {
+      john.angle = 70
+      john.y = john.y -10;
+    }
+    
+    if(keyDown(DOWN_ARROW))
+    {
+      john.angle = -70
+      john.y = john.y +10;
+    }
+
+    if(keyDown(RIGHT_ARROW))
+    {
+      john.changeAnimation("swimming",swimming) 
+  john.x = john.x + 8
+
+  //right facing net instructions
+  netsprite.x = john.x + 100;
+  netsprite.y = john.y+50;
+  netsprite.changeImage('netright',netrightimage);
+    }
+
+}
