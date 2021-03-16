08ca434c4cc6825cfbfb343a38c34161cba31bf6
Great4Geek <67558194+Great4Geek@users.noreply.github.com>
2021-03-16 15:24:51 -0400

    Update gamestate2.js

--- gamestate2.js
+++ gamestate2.js
@@ -60,7 +60,7 @@ if(keyDown(LEFT_ARROW))
   john.x = john.x - 8
 
   //right facing net instructions
-  netsprite.x = john.x-10 ;
+  netsprite.x = john.x-40 ;
   netsprite.y = john.y-10;
   netsprite.changeImage("netleft",netimage);
 
