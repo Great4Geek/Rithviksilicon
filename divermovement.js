6ff3cbfef6b550031d70f9998c0c398571361ca0
Great4Geek <67558194+Great4Geek@users.noreply.github.com>
2021-03-16 12:41:52 -0400

    Update divermovement.js

--- divermovement.js
+++ divermovement.js
@@ -6,7 +6,7 @@ function diver_moves()
       john.changeAnimation("swimmingleft",swimmingleft)
       netsprite.changeImage('netleft',netimage)
       john.x = john.x - 8
-      netsprite.x = john.x - 40;
+      netsprite.x = john.x - 10;
       netsprite.y = john.y+50
       
     }
