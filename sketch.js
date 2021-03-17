01491a573ec321311da797102f5f8454b50f0eb4
Great4Geek <67558194+Great4Geek@users.noreply.github.com>
2021-03-16 21:22:35 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -49,7 +49,7 @@ turtlepic = loadImage("images/turtlepic.jpg")
 }
 
 function setup() {
-	createCanvas(displayWidth,displayHeight/1.1);
+	createCanvas(displayWidth/0.9,displayHeight/0.9);
 	background(200);
 
   sadboi = createSprite(displayWidth/2,displayHeight/2,100,100)
